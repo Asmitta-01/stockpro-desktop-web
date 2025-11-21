5568b74d151f6f77330e5d2314e0447f26ca072a
Asmitta <tiwabrayan@gmail.com>
2025-11-21 12:17:43 +0100

    Add title to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,7 @@ import { DownloadPage } from './pages/download-page/download-page';
 
 export const routes: Routes = [
     { path: '', component: LandingPage },
-    { path: 'features', component: FeaturesPage },
-    { path: 'reasons', component: ReasonsPage },
-    { path: 'download', component: DownloadPage }
+    { path: 'features', component: FeaturesPage, title: 'Fonctionnalités de StockPro Desktop' },
+    { path: 'reasons', component: ReasonsPage, title: 'Pourquoi choisir StockPro' },
+    { path: 'download', component: DownloadPage, title: 'Téléchargement de StockPro' }
 ];
