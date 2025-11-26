d71793e343725510fc62a6fcdec4278f0be82aab
Asmitta <tiwabrayan@gmail.com>
2025-11-26 10:16:58 +0100

    Fix broken links

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -9,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 export class Footer implements OnInit {
 
   logo!: string;
+  formUrl!: string;
 
   ngOnInit(): void {
     this.logo = 'logo-147x32.png';
+    this.formUrl = 'https://tech.us19.list-manage.com/subscribe/post?u=6761a6214f504cd48b5a248c0&amp;id=6705a369aa&amp;f_id=003d21e7f0';
   }
 
 }
