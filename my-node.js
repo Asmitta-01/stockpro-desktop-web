301a06e31315741ef61512a3ccf141f76a1de6bd
Asmitta <tiwabrayan@gmail.com>
2025-11-26 12:50:05 +0100

    Add dotenv support and environment file generation

---
+++ my-node.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const successColor = '\x1b[32m%s\x1b[0m';
+const checkSign = '\u{2705}';
+const dotenv = require('dotenv').config({ path: 'src/.env' });;
+
+const envFile = `export const environment = {
+    supabaseUrl: '${process.env.SUPABASE_URL}',
+    supabaseKey: '${process.env.SUPABASE_KEY}',
+    notchpayPublicKey: '${process.env.NOTCHPAY_PUBLIC_KEY}',
+};
+`;
+const targetPath = path.join(__dirname, './src/environments/environment.development.ts');
+fs.writeFile(targetPath, envFile, (err) => {
+    if (err) {
+        console.error(err);
+        throw err;
+    } else {
+        console.log(successColor, `${checkSign} Successfully generated environment.development.ts`);
+    }
+})
