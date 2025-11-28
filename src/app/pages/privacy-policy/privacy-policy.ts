ec2dbca6b0a56251966bbf60a74c6ec9fa299541
Asmitta <tiwabrayan@gmail.com>
2025-11-28 10:22:00 +0100

    Fix http request issue in privacy-poly during build

--- src/app/pages/privacy-policy/privacy-policy.ts
+++ src/app/pages/privacy-policy/privacy-policy.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { Navbar } from "../../components/navbar/navbar";
 import { SubpageHeader } from "../../components/subpage-header/subpage-header";
 import { HttpClient } from '@angular/common/http';
@@ -8,18 +8,33 @@ import { HttpClient } from '@angular/common/http';
   selector: 'app-privacy-policy',
   imports: [CommonModule, Navbar, SubpageHeader],
   templateUrl: './privacy-policy.html',
-  styleUrl: './privacy-policy.css',
+  styleUrls: ['./privacy-policy.css'],
 })
 export class PrivacyPolicyPage implements OnInit {
   markdownContent = '';
 
-  constructor(private http: HttpClient) { };
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) { };
 
   ngOnInit() {
-    this.http.get('docs/privacy-policy.md', { responseType: 'text' })
-      .subscribe(html => {
-        this.markdownContent = this.parseMarkdown(html);
-      });
+    // Only attempt to fetch the local markdown file in the browser.
+    // During server-side prerender the relative HTTP request will fail
+    // and cause the build to error (uncaughtException). Skipping the
+    // request during SSR prevents that runtime error.
+    if (isPlatformBrowser(this.platformId)) {
+      this.http.get('docs/privacy-policy.md', { responseType: 'text' })
+        .subscribe(html => {
+          this.markdownContent = this.parseMarkdown(html);
+        }, err => {
+          console.warn('Unable to load privacy policy:', err);
+          this.markdownContent = '';
+        });
+    } else {
+      // Server-side: do not fetch. Leave content empty (client will load).
+      this.markdownContent = '';
+    }
   }
 
   private parseMarkdown(markdown: string): string {
